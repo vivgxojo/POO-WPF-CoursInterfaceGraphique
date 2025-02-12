c6829221a0d5dd0d8d772eea15f48e8eb7e53f6c
Maryse M <Maryse.Mongeau@cegepoutaouais.qc.ca>
2025-02-12 11:43:25 -0500

    Ajoutez des fichiers projet.

---
+++ CoursInterfaceGraphique/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoursInterfaceGraphique
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gestionnaire d'évènement du bouton soumettre
+        /// Va récupérer le nom et afficher bonjour
+        /// </summary>
+        /// <param name="sender">Bouton qui a été cliqué</param>
+        /// <param name="e">Évènement de clic</param>
+        private void btnSoumettre_Click(object sender, RoutedEventArgs e)
+        {
+            string Nom = txtNom.Text; //Accéder au textbox
+            lblBonjour.Content = "Bonjour " + Nom; //Modifier le texte du label
+        }
+    }
+}
